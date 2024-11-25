326c8d018b89a1e309ffe43e3137914f6ceb6222
Yves Leung-Tack <yves.leung-tack@montreal.ca>
2024-11-25 13:36:29 -0500

    feat(section2-7:8): modif section 2.7 a 2.8

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>("/api/courses").pipe(
+      map((response) => response["payload"]),
+      shareReplay()
+    );
+  }
+}
